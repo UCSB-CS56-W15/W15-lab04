2a0eeb4bf765e950103e4330e0af5355781710ba
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2015-02-07 22:04:11 -0800

    added javadoc style comments

--- src/edu/ucsb/cs56/w15/drawings/nikolas_chaconas/advanced/BalloonsTiedToHouse.java
+++ src/edu/ucsb/cs56/w15/drawings/nikolas_chaconas/advanced/BalloonsTiedToHouse.java
@@ -17,7 +17,7 @@ import edu.ucsb.cs56.w15.drawings.utilities.ShapeTransforms;
 import edu.ucsb.cs56.w15.drawings.utilities.GeneralPathWrapper;
 
 /**
-   A drawing of a bunch of balloons made by my Balloons class being held by a stick figure
+   A drawing of a bunch of balloons made by my Balloons class tied to a house
    BalloonsTiedToHouse extends my Balloons class
    @author Nikolas Chaconas
    @version for CS56, Winter 15, UCSB
@@ -25,20 +25,36 @@ import edu.ucsb.cs56.w15.drawings.utilities.GeneralPathWrapper;
 */
 public class BalloonsTiedToHouse extends Balloons implements Shape
 {
-
+	/**
+	       Constructor to call Balloons and to also draw the house for balloons to be attached to.
+	       @param x, x coordinate of center of balloon
+	       @param y, y coordinate of center of balloon
+	       @param size, proportional multiplier for width radius of balloon 1 is small, 10 is large
+	       @param xCord, x coordinate of bottom of string
+	       @param yCord, y coordinate of bottom of string
+	*/
+		
 	public BalloonsTiedToHouse(double x, double y, double size, double xCord, double yCord) {
 		
 		super(x,y,size,xCord,yCord);
 		
 		double length = size*250;
 		double width = size*200;
+
 		//make top of house using inheritted class MakeTriangle from Balloons.java
 		GeneralPath topOfHouse = MakeTriangle(x, yCord-length, width, length);
 		
+		//base of house x coordinate
 		double basey = yCord-length+1.1*length;
+		//base of house y coordinate
 		double basex = x-width/5;
+		
+		//height of base of house
 		double baseHeight = width*1/3;
+
+		//width of base of house
 		double baseWidth = width*2/5;
+
 		Rectangle2D.Double base =
 	    		new Rectangle2D.Double(basex, basey,baseWidth, baseHeight);
 
