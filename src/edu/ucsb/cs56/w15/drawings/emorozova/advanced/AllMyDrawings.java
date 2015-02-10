89541a6185dd84be60c80e99a09d2235f07075f7
Elena Morozova <emorozova@umail.ucsb.edu>
2015-02-10 15:09:12 -0800

    Drew drawings 1 2 and 3

--- src/edu/ucsb/cs56/w15/drawings/emorozova/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/w15/drawings/emorozova/advanced/AllMyDrawings.java
@@ -31,20 +31,22 @@ public class AllMyDrawings
 
     public static void drawPicture1(Graphics2D g2) {
 
-	Lighthouse h1 = new Lighthouse(100,250,150,250);
-	g2.setColor(Color.BLACK); g2.draw(h1);
+	Lighthouse h1 = new Lighthouse(100,400,80,210);
+	g2.setColor(Color.BLACK); 
+	g2.draw(h1);
 	
-	// Make a black house that's half the size, 
+	// Make a red lighthouse that's half the size, 
 	// and moved over 150 pixels in x direction
 
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1, 0.5, 0.5);
+	h2 = ShapeTransforms.translatedCopyOf(h2, 150, 0);
+	g2.setColor(Color.RED);
+	g2.draw(h2);
 	
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a house that's 3x as big (1.5x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	h2 = ShapeTransforms.scaledCopyOfLL(h2,3,3);
+	h2  = ShapeTransforms.translatedCopyOf(h2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -58,40 +60,34 @@ public class AllMyDrawings
 	g2.setColor(new Color(0x002FA7)); 
 	g2.draw(h2); 
 	
-	// Draw two houses with Windows
-	/*
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
-	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
-	*/
-	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Phill Conrad", 20,20);
+	g2.drawString("A few basic lighthouses by Elena Morozova", 20,20);
     }
 
 
-    /** Draw a picture with a few houses and coffee cups
+    /** Draw a picture with a few different lighthouses
      */
     public static void drawPicture2(Graphics2D g2) {
 
-Lighthouse h1 = new Lighthouse(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	Lighthouse h1 = new Lighthouse(100,400,50,190);
+	g2.setColor(Color.BLACK); g2.draw(h1);
+
+	StripedLighthouse s1 = new StripedLighthouse(200,400,80,190);
+	g2.setColor(Color.PINK); g2.draw(s1);
 	
 	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
+	// and moved over 180 pixels in x direction
 
 	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	h2 = ShapeTransforms.translatedCopyOf(h2,190,0);
+	g2.setColor(Color.RED); g2.draw(h2);
 	
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a striped house that's 2x the height of the original
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
+	h2 = ShapeTransforms.scaledCopyOfLL(s1,1,2);
 	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
 	
 	// We'll draw this with a thicker stroke
@@ -106,20 +102,11 @@ Lighthouse h1 = new Lighthouse(100,250,50,75);
 	g2.setColor(new Color(0x002FA7)); 
 	g2.draw(h2); 
 	
-	// Draw two houses with Windows
-	/*
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
-	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
-	*/
-	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Phill Conrad", 20,20);
+	g2.drawString("A few different lighthouses by Elena Morozova", 20,20);
     }
   
     /** Draw a different picture with a few houses and coffee cups
@@ -127,47 +114,15 @@ Lighthouse h1 = new Lighthouse(100,250,50,75);
 
     public static void drawPicture3(Graphics2D g2) {
 	
-Lighthouse h1 = new Lighthouse(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
-	
-	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
+	StripedLighthouse h1 = new StripedLighthouse(100,250,50,160);
+	g2.setColor(Color.BLACK); g2.draw(h1);
 
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
-	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	
-	// We'll draw this with a thicker stroke
-	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
-	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
-	
-	Stroke orig=g2.getStroke();
-	g2.setStroke(thick);
-	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
-	
-	// Draw two houses with Windows
-	/*
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
-	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
-	*/
-	
-	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
-	
-	g2.setStroke(orig);
-	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Phill Conrad", 20,20);
+	Shape h2 = ShapeTransforms.rotatedCopyOf(h1, Math.PI/2.0);
+	h2 = ShapeTransforms.translatedCopyOf(h2, 150, 90);
+	g2.draw(h2);
+
+	g2.drawString("Poorly built lighthouse after a storm by Elena Morozova", 20,20);
+        
        
        
     }
