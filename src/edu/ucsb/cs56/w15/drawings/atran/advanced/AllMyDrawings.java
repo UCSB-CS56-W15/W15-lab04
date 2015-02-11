4461979281cda5f999e3aa78cfad54eed4fdb0ca
Andrew Tran <atran@umail.ucsb.edu>
2015-02-11 12:50:55 -0800

    Added LightBulbWithSwitch, which extends LightBulb, and modified the MPV

--- src/edu/ucsb/cs56/w15/drawings/atran/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/w15/drawings/atran/advanced/AllMyDrawings.java
@@ -31,20 +31,20 @@ public class AllMyDrawings
 
     public static void drawPicture1(Graphics2D g2) {
 
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	LightBulb LB1 = new LightBulb(100,250,50,75);
+	g2.setColor(Color.CYAN); g2.draw(LB1);
 	
-	// Make a black house that's half the size, 
+	// Make a black lightbulb that's half the size, 
 	// and moved over 150 pixels in x direction
 
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape LB2 = ShapeTransforms.scaledCopyOfLL(LB1,0.5,0.5);
+	LB2 = ShapeTransforms.translatedCopyOf(LB2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(LB2);
 	
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a lightbulb that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	LB2 = ShapeTransforms.scaledCopyOfLL(LB2,4,4);
+	LB2 = ShapeTransforms.translatedCopyOf(LB2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -56,21 +56,21 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(LB2); 
 	
-	// Draw two houses with Windows
+	// Draw two lightbulbs with switches
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	LightBulbWithSwitch LBS1 = new LightBulbWithSwitch(50,350,40,75);
+	LightBulbWithSwitch LBS2 = new LightBulbWithSwitch(200,350,200,100);
 	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
+	g2.draw(LBS1);
+	g2.setColor(new Color(0x8F00FF)); g2.draw(LBS2);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Phill Conrad", 20,20);
+	g2.drawString("A few lightbulbs by Andrew Tran", 20,20);
     }
 
 
@@ -78,31 +78,31 @@ public class AllMyDrawings
      */
     public static void drawPicture2(Graphics2D g2) {
 
-	// Draw some coffee cups.
+	// Draw some lightbulbs.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
+	LightBulb large = new LightBulb(100,50,225,150);
+	LightBulb smallLB = new LightBulb(20,50,40,30);
+	LightBulb tallSkinny = new LightBulb(20,150,20,40);
+	LightBulb shortFat = new LightBulb(20,250,40,20);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
+	g2.setColor(Color.GREEN);   g2.draw(smallLB);
 	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
 	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
 	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	LightBulb lb1 = new LightBulb(100,250,50,75);
+	g2.setColor(Color.CYAN); g2.draw(lb1);
 	
-	// Make a black house that's half the size, 
+	// Make a black lightbulb that's half the size, 
 	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape lb2 = ShapeTransforms.scaledCopyOfLL(lb1,0.5,0.5);
+	lb2 = ShapeTransforms.translatedCopyOf(lb2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(lb2);
 	
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a lightbulb that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	lb2 = ShapeTransforms.scaledCopyOfLL(lb2,4,4);
+	lb2 = ShapeTransforms.translatedCopyOf(lb2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -114,26 +114,26 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(lb2); 
 	
-	// Draw two houses with Windows
+	// Draw two lightbulbs with switches
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	LightBulbWithSwitch lbs1 = new LightBulbWithSwitch(50,350,40,75);
+	LightBulbWithSwitch lbs2 = new LightBulbWithSwitch(200,350,200,100);
 	
-	g2.draw(hw1);
+	g2.draw(lbs1);
 	g2.setColor(new Color(0x8F00FF)); 
 
-	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
+	// Rotate the second lightbulb 45 degrees around its center.
+	Shape lbs3 = ShapeTransforms.rotatedCopyOf(lbs2, Math.PI/4.0);
 
-	g2.draw(hw3);
+	g2.draw(lbs3);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+	g2.drawString("A bunch of LightBulbs and a few with swithces by Andrew Tran", 20,20);
     }
   
     /** Draw a different picture with a few houses and coffee cups
@@ -149,18 +149,18 @@ public class AllMyDrawings
 	
 	// Draw some coffee cups.
 	
-       CoffeeCup large = new CoffeeCup(100,50,225,150);
-       CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-       LightBulb test = new LightBulb(50,150,100,200);
+       LightBulbWithSwitch large = new LightBulbWithSwitch(100,50,225,150);
+       LightBulbWithSwitch smallLB = new LightBulbWithSwitch(20,50,40,30);
        LightBulb gapTest = new LightBulb(150,200,75,400);
        LightBulb widthTest = new LightBulb(50,50,50,50);
+       LightBulbWithSwitch testSwitch = new LightBulbWithSwitch(50,150,100,200);
 
 
        g2.setColor(Color.RED);     g2.draw(large);
-       g2.setColor(Color.GREEN);   g2.draw(smallCC);
-       g2.setColor(Color.BLUE);    g2.draw(test);
+       g2.setColor(Color.GREEN);   g2.draw(smallLB);
        g2.setColor(Color.BLUE);    g2.draw(gapTest);
        g2.draw(widthTest);
+       g2.setColor(Color.RED);     g2.draw(testSwitch);
        
     }
     
