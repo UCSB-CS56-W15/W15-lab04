ee6f67392a1709f5eec5050ab753683b9377bde6
Eric Swenson <elswenson@umail.ucsb.edu>
2015-02-18 15:18:36 -0800

    Fixed R4 vector graphics

--- src/edu/ucsb/cs56/w15/drawings/elswenson/advanced/R4.java
+++ src/edu/ucsb/cs56/w15/drawings/elswenson/advanced/R4.java
@@ -22,23 +22,23 @@ import edu.ucsb.cs56.w15.drawings.utilities.GeneralPathWrapper;
   @version CS56W15 
   */
 
-public class R2 extends Droid implements Shape
+public class R4 extends Droid implements Shape
 {
 	/**
  	 Constructor for the R4 class, creates a R4 Droid.
-	 @param x = lower left x coordinate of the droid
-	 @param y = lower left y coordinate of the droid
+	 @param x = top left x coordinate of the droid
+	 @param y = top left y coordinate of the droid
 	 @param width = width of the droid
 	 @param height = height of the droid
 	*/
-	public R2(double x, double y, double width, double height)
+	public R4(double x, double y, double width, double height)
 	{
-		super(x + 0.05*width, y, 0.9*width, 0.8*height); //gives space above and on both left and right sides.
+		super(x + 0.05*width, y + 0.2 * height, 0.9*width, 0.8*height); //gives space above and on both left and right sides.
 		
 		double domeHeight = 0.2*height;
-		double domeWidth = 0.7*0.7*0.9*width;
-		double domeX = x + 0.9*0.15*width;
-		double domeY = y + domeHeight;
+		double domeTopWidth = .3*width;
+		double domeBottomLeftX = x + (width - 0.7*0.9*width)/2;
+		double domeTopLeftX = x + 0.35*width;
 		
 		double footHeight = 0.1*height;
 		double footWidth = 0.1*width + 0.9*0.15*width;
@@ -46,17 +46,23 @@ public class R2 extends Droid implements Shape
 		double eyeHeight = 0.05*height;
 		double eyeWidth = eyeHeight;
 		double eyeX = x + 0.5*width;
-		double eyeY = domeY + 0.5*domeHeight;
+		double eyeY = y + 0.5*domeHeight;
 
 		Rectangle2D.Double leftFoot = new Rectangle2D.Double(x, y + height, footWidth, footHeight);
 		Rectangle2D.Double rightFoot = new Rectangle2D.Double(x + width - footWidth, y + height, footWidth, footHeight);
-		Arc2D.Double dome = new Arc2D.Double(180, domeHeight, 0, domeWidth, domeX, domeY, Arc2D.CHORD);
 		Ellipse2D.Double eye = new Ellipse2D.Double(eyeX, eyeY, eyeWidth, eyeHeight);		
+	
+		Line2D.Double leftDome = new Line2D.Double(domeBottomLeftX, y + domeHeight, domeTopLeftX, y);
+		Line2D.Double rightDome = new Line2D.Double(domeBottomLeftX + (0.7*0.9*width), y + domeHeight, domeTopLeftX + domeTopWidth, y);
+		Line2D.Double topDome = new Line2D.Double(domeTopLeftX, y, domeTopLeftX + domeTopWidth, y);
+
 
 		GeneralPath thisDroid = this.get();
 		thisDroid.append(leftFoot, false);
 		thisDroid.append(rightFoot, false);
-		thisDroid.append(dome, false);
+		thisDroid.append(leftDome, false);
+		thisDroid.append(rightDome, false);
+		thisDroid.append(topDome, false);
 		thisDroid.append(eye, false);
 		
 	}
