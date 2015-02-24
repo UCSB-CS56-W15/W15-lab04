9698b9aeca570495ff0a5d1bd92a536a1437e1f5
Chloe Pounds <chloepounds@gmail.com>
2015-02-24 13:49:06 -0800

    committing correct files

--- src/edu/ucsb/cs56/w15/drawings/chloepounds/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/w15/drawings/chloepounds/advanced/AllMyDrawings.java
@@ -17,39 +17,41 @@ import edu.ucsb.cs56.w15.drawings.utilities.ShapeTransforms;
 import edu.ucsb.cs56.w15.drawings.utilities.GeneralPathWrapper;
 
 /**
- * A class with static methods for drawing various pictures
+ * A class with static methods for drawing kids and kids with balloons
  * 
- * @author Phill Conrad 
- * @version for CS10, lab06, Spring 2009
+ * @author Chloe Pounds
+ * @version for CS56, lab04, Winter 2015
  */
-
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few houses 
+    /** Draw a picture of 3 kids and 3 kids with a balloon.  
      */
-
     public static void drawPicture1(Graphics2D g2) {
+	
+	//Create and draw a blue kid. 
 
-	Kid k1 = new Kid(100,200,50,-100);
+	Kid k1 = new Kid(100,350,50,-100);
 	g2.setColor(Color.BLUE); g2.draw(k1);
 	
-	// Make a black house that's half the size, 
+	// Make a green kid that's half the size, 
 	// and moved over 150 pixels in x direction
 
 	Shape k2 = ShapeTransforms.scaledCopyOfLL(k1,0.5,0.5);
 	k2 = ShapeTransforms.translatedCopyOf(k2,150,0);
 	g2.setColor(Color.GREEN); g2.draw(k2);
 	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	k2 = ShapeTransforms.scaledCopyOfLL(k2,4,4);
-	k2 = ShapeTransforms.translatedCopyOf(k2,150,0);
+	// A kid rescaled 3x the size of the original and moved 100 pixels over
+
+	k2 = ShapeTransforms.scaledCopyOfLL(k2,6,6);
+	k2 = ShapeTransforms.translatedCopyOf(k2,100,0);
 	
-	// We'll draw this with a thicker stroke
-	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
+	//k2 drawn with a thicker stroke 
+	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, 
+					BasicStroke.JOIN_BEVEL);       
 	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
+	// for hex colors, see (e.g.)
+	// http://en.wikipedia.org/wiki/List_of_colors
 	// #002FA7 is "International Klein Blue" according to Wikipedia
 	// In HTML we use #, but in Java (and C/C++) its 0x
 	
@@ -58,105 +60,51 @@ public class AllMyDrawings
 	g2.setColor(Color.PINK); 
 	g2.draw(k2); 
 	
-	// Draw two houses with Windows
-	
-	//HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	//HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
-	
-	//g2.draw(hw1);
-	//g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
-	
-	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
-	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
 	g2.drawString("A few kids by Chloe Pounds", 20,20);
+
+	//draw a kid with a balloon
+    	KidWithBalloon chloe = new KidWithBalloon(100,200,50,-100);
+	g2.setColor(Color.MAGENTA);
+	g2.draw(chloe);
+
+	//rotate chloe 45 degree around its center and move her 100 pixels 
+	//to the left and 50 pixels up
+	Shape hanna = ShapeTransforms.rotatedCopyOf(chloe, Math.PI/4.0);
+	hanna = ShapeTransforms.translatedCopyOf(hanna, 100, -50);
+	g2.setColor(Color.orange);
+	g2.draw(hanna);
+	
+	//move hanna 300 pixels to the right and 50 pixels down
+	Shape kelsey = ShapeTransforms.translatedCopyOf(hanna, 300, 50);
+	g2.setColor(Color.yellow);
+	g2.draw(kelsey);
+
     }
 
-
-    /** Draw a picture with a few houses and coffee cups
-     */
-    /*
     public static void drawPicture2(Graphics2D g2) {
+	KidWithBalloon k1 = new KidWithBalloon(100,450,100,-200);
+        g2.setColor(Color.BLUE); g2.draw(k1);
 
-	// Draw some coffee cups.
-	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
-	
-	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
-	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
-	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
-	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
-	
-	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
-	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	
-	// We'll draw this with a thicker stroke
-	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
-	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
-	
-	Stroke orig=g2.getStroke();
-	g2.setStroke(thick);
-	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
-	
-	// Draw two houses with Windows
-	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
-	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); 
+	Shape k2 = ShapeTransforms.scaledCopyOfLL(k1,0.5,0.5);
+	k2 = ShapeTransforms.translatedCopyOf(k2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(k2);
 
-	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
 
-	g2.draw(hw3);
-	
-	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
-	
-	g2.setStroke(orig);
-	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
     }
-  */
-    /** Draw a different picture with a few houses and coffee cups
-     */
-/*
-    public static void drawPicture3(Graphics2D g2) {
-	
-	// label the drawing
-	
-	g2.drawString("A bunch of Coffee Cups by Phill Conrad", 20,20);
 
-	
-	// Draw some coffee cups.
-	
-       CoffeeCup large = new CoffeeCup(100,50,225,150);
-       CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-       
-       g2.setColor(Color.RED);     g2.draw(large);
-       g2.setColor(Color.GREEN);   g2.draw(smallCC);
-       
-       
+    public static void drawPicture3(Graphics2D g3) {
+	Kid k1 = new Kid(0,350,75,-100);
+        g3.setColor(Color.pink); g3.draw(k1);
+    
+	KidWithBalloon k2 = new KidWithBalloon(100,350,75,-100);
+	g3.draw(k2);
+
+
+	Shape k3 = ShapeTransforms.scaledCopyOfLL(k2,0.5,0.5);
+	k3 = ShapeTransforms.translatedCopyOf(k3,150,0);
+	g3.setColor(Color.orange); g3.draw(k3);
     }
- */   
-
+    
 }
