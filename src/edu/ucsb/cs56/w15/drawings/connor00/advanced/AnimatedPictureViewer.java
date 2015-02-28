395f1e4e66659857d0ca38e29a0df0c0d85518f3
Connor Armbrust Mulcahey <connormulcahey@me.com>
2015-02-27 16:57:14 -0800

    finished animation

--- src/edu/ucsb/cs56/w15/drawings/connor00/advanced/AnimatedPictureViewer.java
+++ src/edu/ucsb/cs56/w15/drawings/connor00/advanced/AnimatedPictureViewer.java
@@ -4,49 +4,30 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+import edu.ucsb.cs56.w15.drawings.utilities.ShapeTransforms;
+
 public class AnimatedPictureViewer {
 
     private DrawPanel panel = new DrawPanel();
     
-    private Ipod ipod = new Ipod(100, 100, 100);
+    private ParabolicStar star = new ParabolicStar(0, 0, 500, 500, 1);
+    private ParabolicStar star2 = new ParabolicStar(0, 0, 500, 500, 1);
     
-    Thread anim;   
+    Thread anim;
     
-    private int x = 100;
-    private int y = 100;
-    
-    private int dx = 5;
+    private int x = 0;
+    private int y = 0;
+    private double theta = 0.0; 
+
+    private int dl = 1;
+    private int ds = 5;
+    private double dt = Math.PI / 45.0;
+
+    private int lines = 1;
 
     public static void main (String[] args) {
       new AnimatedPictureViewer().go();
     }
-
-    public void go() {
-      JFrame frame = new JFrame();
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-      frame.getContentPane().add(panel);
-      frame.setSize(640,480);
-      frame.setVisible(true);
-      
-      frame.getContentPane().addMouseListener(new MouseAdapter() {
-        public void mouseEntered(MouseEvent e){
-        System.out.println("mouse entered");
-          anim = new Animation();
-          anim.start();
-        }
-
-        public void mouseExited(MouseEvent e){        
-          System.out.println("Mouse exited");
-          // Kill the animation thread
-          anim.interrupt();
-          while (anim.isAlive()){}
-          anim = null;         
-          panel.repaint();        
-        }
-      });
-      
-    } // go()
 
     class DrawPanel extends JPanel {
        public void paintComponent(Graphics g) {
@@ -57,10 +38,21 @@ public class AnimatedPictureViewer {
           g2.setColor(Color.white);
           g2.fillRect(0,0,this.getWidth(), this.getHeight());
 
-          // Draw the Ipod
-          g2.setColor(Color.RED);
-          Ipod test = new Ipod(x, y, 100);
-          g2.draw(test);
+          // Draw the Star
+	  
+          ParabolicStar base = new ParabolicStar(x, y, 700 - (x * 2), 700 - (y * 2), lines);
+	  Shape star1paint = ShapeTransforms.rotatedCopyOf(base, theta);
+	  Shape star2paint = ShapeTransforms.rotatedCopyOf(base, Math.PI/4 - theta);
+	  Shape star3paint = ShapeTransforms.rotatedCopyOf(base, Math.PI/3 + theta);
+	  Shape star4paint = ShapeTransforms.rotatedCopyOf(base, Math.PI/6 - theta);
+	  g2.setColor(new Color(0xDD0000));
+	  g2.draw(star1paint);
+	  g2.setColor(new Color(0x000099));
+	  g2.draw(star2paint);
+	  g2.setColor(new Color(0x009900));
+	  g2.draw(star3paint);
+	  g2.setColor(new Color(0xDDDD00));
+	  g2.draw(star4paint);
        }
     }
     
@@ -68,14 +60,23 @@ public class AnimatedPictureViewer {
       public void run() {
         try {
           while (true) {
-            // Bounce off the walls
 
-            if (x >= 400) { dx = -5; }
-            if (x <= 50) { dx = 5; }
-            
-            x += dx;                
+            if (x >= 350) { 
+		ds = -5;
+		dl = -1;
+	    }
+            if (x <= 5) { 
+		ds = 5;
+		dl = 1;
+	    }
+
+            x += ds;
+	    y += ds;
+	    lines += dl;
+	    theta += dt;
+
             panel.repaint();
-            Thread.sleep(50);
+            Thread.sleep(100 - lines);
           }
         } catch(Exception ex) {
           if (ex instanceof InterruptedException) {
@@ -88,4 +89,30 @@ public class AnimatedPictureViewer {
       }
     }
     
+    public void go() {
+	    JFrame frame = new JFrame();
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
+	    frame.getContentPane().add(panel);
+	    frame.setSize(640,480);
+	    frame.setVisible(true);
+	    
+	    frame.getContentPane().addMouseListener(new MouseAdapter() {
+		    public void mouseEntered(MouseEvent e){
+			System.out.println("mouse entered");
+			anim = new Animation();
+			anim.start();
+		    }
+		    
+		    public void mouseExited(MouseEvent e){        
+			System.out.println("Mouse exited");
+			// Kill the animation thread
+			anim.interrupt();
+			while (anim.isAlive()){}
+			anim = null;         
+			panel.repaint();        
+		    }
+		});
+	    
+    } // go()
 }
